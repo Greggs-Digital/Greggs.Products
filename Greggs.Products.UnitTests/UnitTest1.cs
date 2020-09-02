18516413064f3024fdce9aa78fa70bf2a0779964
Lloyd Powell <lloyd.powell@greggs.co.uk>
2020-09-02 11:06:01 +0100

    Add swagger

---
+++ Greggs.Products.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Greggs.Products.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            throw new NotImplementedException("We have no tests :-(");
+        }
+    }
+}
