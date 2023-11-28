75e8b69c5f01d5ff6b58841dd9eead4b697f77d4
Relishiya <57314295+Relishiya@users.noreply.github.com>
2023-11-28 13:42:10 +0000

    User Story Implementation by Relishiya

--- Greggs.Products.Api/DataAccess/ProductAccess.cs
+++ Greggs.Products.Api/DataAccess/ProductAccess.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using Greggs.Products.Api.Models;
-
 namespace Greggs.Products.Api.DataAccess;
+
 
 /// <summary>
 /// DISCLAIMER: This is only here to help enable the purpose of this exercise, this doesn't reflect the way we work!
 /// </summary>
 public class ProductAccess : IDataAccess<Product>
 {
+ 
     private static readonly IEnumerable<Product> ProductDatabase = new List<Product>()
     {
         new() { Name = "Sausage Roll", PriceInPounds = 1m },
@@ -19,7 +20,8 @@ public class ProductAccess : IDataAccess<Product>
         new() { Name = "Mexican Baguette", PriceInPounds = 2.1m },
         new() { Name = "Bacon Sandwich", PriceInPounds = 1.95m },
         new() { Name = "Coca Cola", PriceInPounds = 1.2m }
-    };
+    };    
+
 
     public IEnumerable<Product> List(int? pageStart, int? pageSize)
     {
